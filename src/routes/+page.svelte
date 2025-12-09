a606ddef11749eabe7bc7337a3bf30098129f887
RenzoFOL <renzo.ortizlara@gmail.com>
2025-12-08 21:54:27 -0600

    Resolviendo produccion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
   import Login from '$lib/components/inicio-registro/Login.svelte';
 </script>
-
 <h1 style="text-align:center; margin-top:2rem;">ParkSistem</h1>
 
 <div style="display:flex; justify-content:center; margin-top:1.5rem;">
   <Login />
 </div>
+
